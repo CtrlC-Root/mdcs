af794ac22c27a68952cf38f7846c0ee1ed5163f4
Alexandru Barbur <root.ctrlc@gmail.com>
2019-09-22 00:48:14 -0400

    Replace device structs with traits.

--- pkg/mdcs/src/device.rs
+++ pkg/mdcs/src/device.rs
@@ -1,24 +1,47 @@
+use std::fmt;
 use std::collections::HashMap;
 
-#[derive(Debug)]
-pub struct Attribute {
-    pub schema: String
+pub trait Attribute {
+    fn schema(&self) -> &str;
+    // fn read(&self);
+    // fn write(&self);
 }
 
-#[derive(Debug)]
-pub struct Action {
-    pub input_schema: String,
-    pub output_schema: String
+impl fmt::Debug for &dyn Attribute {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.debug_struct("Attribute")
+            .field("schema", &self.schema())
+            .finish()
+    }
 }
 
-#[derive(Debug)]
-pub enum Member {
-    Attribute(Attribute),
-    Action(Action)
+pub trait Action {
+    fn input_schema(&self) -> &str;
+    fn output_schema(&self) -> &str;
+    // fn run(&self);
 }
 
-#[derive(Debug)]
-pub struct Device {
-    pub name: String,
-    pub members: HashMap<String, Member>
+impl fmt::Debug for &dyn Action {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.debug_struct("Action")
+            .field("input_schema", &self.input_schema())
+            .field("output_schema", &self.output_schema())
+            .finish()
+    }
+}
+
+pub trait Device {
+    fn name(&self) -> &str;
+    fn attributes(&self) -> &HashMap<&str, &dyn Attribute>;
+    fn actions(&self) -> &HashMap<&str, &dyn Action>;
+}
+
+impl fmt::Debug for &dyn Device {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.debug_struct("Device")
+            .field("name", &self.name())
+            .field("attributes", &self.attributes())
+            .field("actions", &self.actions())
+            .finish()
+    }
 }
--- pkg/mdcs_node/src/node.rs
+++ pkg/mdcs_node/src/node.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use mdcs::device::Device;
 
 #[derive(Debug)]
-pub struct Node {
+pub struct Node<'a> {
     pub name: String,
-    pub devices: HashMap<String, Device>
+    pub devices: HashMap<String, &'a dyn Device>
 }
