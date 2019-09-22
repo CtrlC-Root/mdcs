747ef92a701060edbd02e2dcc9cef4d31a97b938
Alexandru Barbur <root.ctrlc@gmail.com>
2019-09-22 02:10:43 -0400

    Debug implementation of generic types.

--- pkg/mdcs_node/src/main.rs
+++ pkg/mdcs_node/src/main.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+use mdcs::device;
+use mdcs_node::node::{Attribute, Action, Member, Device};
+
 fn main() {
-    println!("Hello, world!");
+    let serial = Attribute {
+        schema: String::from("string")
+    };
+
+    let beep = Action {
+        input_schema: String::from("int"),
+        output_schema: String::from("int")
+    };
+
+    let mut light = Device {
+        name: String::from("light"),
+        members: HashMap::new()
+    };
+
+    light.members.insert(String::from("serial"), Member::Attribute(serial));
+    light.members.insert(String::from("beep"), Member::Action(beep));
+
+    println!("{:#?}\n", light);
+
+    let device: &dyn device::Device = &light;
+    println!("{:#?}", device);
 }
--- pkg/mdcs_node/src/node.rs
+++ pkg/mdcs_node/src/node.rs
@@ -1,8 +1,59 @@
 use std::collections::HashMap;
-use mdcs::device::Device;
+use mdcs::device;
 
 #[derive(Debug)]
-pub struct Node<'a> {
+pub struct Attribute {
+    pub schema: String
+}
+
+impl device::Attribute for Attribute {
+    fn schema(&self) -> &str {
+        &self.schema
+    }
+}
+
+#[derive(Debug)]
+pub struct Action {
+    pub input_schema: String,
+    pub output_schema: String
+}
+
+impl device::Action for Action {
+    fn input_schema(&self) -> &str {
+        &self.input_schema
+    }
+
+    fn output_schema(&self) -> &str {
+        &self.output_schema
+    }
+}
+
+#[derive(Debug)]
+pub enum Member {
+    Attribute(Attribute),
+    Action(Action)
+}
+
+#[derive(Debug)]
+pub struct Device {
     pub name: String,
-    pub devices: HashMap<String, &'a dyn Device>
+    pub members: HashMap<String, Member>
+}
+
+impl device::Device for Device {
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    fn members(&self) -> HashMap<&str, device::Member> {
+        self.members
+            .iter()
+            .map(|(ref path, ref member)| {
+                match member {
+                    Member::Attribute(attribute) => (&path[..], device::Member::Attribute(attribute)),
+                    Member::Action(action) => (&path[..], device::Member::Action(action))
+                }
+            })
+            .collect()
+    }
 }
