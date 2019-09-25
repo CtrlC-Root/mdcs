37f4df90ab0e12d07e7bab3f941a90aeadac39ea
Alexandru Barbur <root.ctrlc@gmail.com>
2019-09-24 23:05:41 -0400

    Create a device and a plugin server in the host plugin.

--- pkg/mdcs_node_host/src/main.rs
+++ pkg/mdcs_node_host/src/main.rs
@@ -1,7 +1,12 @@
 use mdcs::device::Device;
+use mdcs_node::plugin::PluginServer;
+
 use sensors;
 
 fn main() {
     let device = Device::new();
     println!("{:#?}", device);
+
+    let server = PluginServer::new(device);
+    server.run();
 }
