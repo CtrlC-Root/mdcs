b90b4859ec936defd64c33ce3323daa6db5e69e5
Alexandru Barbur <root.ctrlc@gmail.com>
2019-10-04 02:20:15 -0400

    Initial plugin server request response flow.

--- pkg/mdcs_node/src/plugin/process.rs
+++ pkg/mdcs_node/src/plugin/process.rs
@@ -3,7 +3,16 @@ use std::error::Error;
 use std::result::Result;
 use std::net::{TcpListener, TcpStream};
 
+use avro_rs::{
+    Schema,
+    Reader,
+    Writer,
+    from_value
+};
+
 use mdcs::device::Device;
+use super::request::{self, PluginRequest};
+use super::response::{self, PluginResponse};
 
 pub struct PluginServer {
     device: Device
@@ -30,11 +39,46 @@ impl PluginServer {
         }
     }
 
-    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
-        let stream = initial_connect()?;
-        stream.set_nonblocking(true)?;
+    fn process_request(&mut self, request: &PluginRequest) -> PluginResponse {
+        // TODO: implement me
+        PluginResponse::Status(response::Status::Ok)
+    }
 
-        // TODO: process requests
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        // parse the plugin message schemas
+        let request_schema = Schema::parse_str(include_str!("request.avsc"))
+            .expect("Failed to parse request message schema");
+
+        let response_schema = Schema::parse_str(include_str!("response.avsc"))
+            .expect("Failed to parse response message schema");
+
+        // wait for someone to connect
+        let stream = initial_connect()?;
+
+        // create plugin request reader and response writer
+        let reader = Reader::with_schema(&request_schema, &stream)?;
+        let mut writer = Writer::new(&response_schema, &stream);
+
+        // process requests
+        for value in reader {
+            let value = value.expect("Failed to read request");
+            let request = from_value::<PluginRequest>(&value);
+
+            let response = match request {
+                Ok(request) => {
+                    self.process_request(&request)
+                },
+                Err(error) => PluginResponse::Error(response::Error {
+                    message: String::from(format!("{}", error)),
+                    path: None
+                })
+            };
+
+            writer.append_ser(response).expect("Failed to serialize response");
+            writer.flush().expect("Failed to flush Avro writer");
+        }
+
+        // quit
         Ok(())
     }
 }
