a733f848c6db7d41ee03593e61c6f36dde2cb3f4
Alexandru Barbur <root.ctrlc@gmail.com>
2019-10-04 19:38:23 -0400

    Add initial PluginServer request processing methods.

--- pkg/mdcs_node/src/plugin/process.rs
+++ pkg/mdcs_node/src/plugin/process.rs
@@ -7,15 +7,17 @@ use avro_rs::{
     Schema,
     Reader,
     Writer,
-    from_value
+    from_value,
+    to_avro_datum
 };
 
-use mdcs::device::Device;
-use super::request::{self, PluginRequest};
-use super::response::{self, PluginResponse};
+use mdcs::device::{Member, Device};
+use super::request::{self as req, PluginRequest};
+use super::response::{self as resp, PluginResponse};
 
 pub struct PluginServer {
-    device: Device
+    device: Device,
+    signal_quit: bool
 }
 
 fn initial_connect() -> io::Result<TcpStream> {
@@ -35,13 +37,60 @@ fn initial_connect() -> io::Result<TcpStream> {
 impl PluginServer {
     pub fn new(device: Device) -> PluginServer {
         PluginServer {
-            device
+            device,
+            signal_quit: false
         }
     }
 
+    fn signal(&mut self, signal: &req::Signal) -> PluginResponse {
+        match signal {
+            req::Signal::Quit => {
+                self.signal_quit = true;
+                PluginResponse::Status(resp::Status::Ok)
+            }
+        }
+    }
+
+    fn describe_device(&mut self) -> PluginResponse {
+        // TODO: implement this
+        PluginResponse::Error(resp::Error {
+            message: String::from("Not Implemented"),
+            path: None
+        })
+    }
+
+    fn read_attribute(&mut self, args: &req::ReadAttribute) -> PluginResponse {
+        // TODO: implement this
+        PluginResponse::Error(resp::Error {
+            message: String::from("Not Implemented"),
+            path: Some(args.path.clone())
+        })
+    }
+
+    fn write_attribute(&mut self, args: &req::WriteAttribute) -> PluginResponse {
+        // TODO: implement this
+        PluginResponse::Error(resp::Error {
+            message: String::from("Not Implemented"),
+            path: Some(args.path.clone())
+        })
+    }
+
+    fn run_action(&mut self, args: &req::RunAction) -> PluginResponse {
+        // TODO: implement this
+        PluginResponse::Error(resp::Error {
+            message: String::from("Not Implemented"),
+            path: Some(args.path.clone())
+        })
+    }
+
     fn process_request(&mut self, request: &PluginRequest) -> PluginResponse {
-        // TODO: implement me
-        PluginResponse::Status(response::Status::Ok)
+        match request {
+            PluginRequest::Signal(signal) => self.signal(signal),
+            PluginRequest::DescribeDevice(_) => self.describe_device(),
+            PluginRequest::ReadAttribute(args) => self.read_attribute(args),
+            PluginRequest::WriteAttribute(args) => self.write_attribute(args),
+            PluginRequest::RunAction(args) => self.run_action(args),
+        }
     }
 
     pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
@@ -61,24 +110,33 @@ impl PluginServer {
 
         // process requests
         for value in reader {
+            // parse the request
             let value = value.expect("Failed to read request");
             let request = from_value::<PluginRequest>(&value);
 
+            // process the request into a response
             let response = match request {
                 Ok(request) => {
                     self.process_request(&request)
                 },
-                Err(error) => PluginResponse::Error(response::Error {
+                Err(error) => PluginResponse::Error(resp::Error {
                     message: String::from(format!("{}", error)),
                     path: None
                 })
             };
 
+            // send the response
             writer.append_ser(response).expect("Failed to serialize response");
             writer.flush().expect("Failed to flush Avro writer");
+
+            // quit if necessary
+            if self.signal_quit {
+                println!("QUIT");
+                break;
+            }
         }
 
-        // quit
+        // finished
         Ok(())
     }
 }
