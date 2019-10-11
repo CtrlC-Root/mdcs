2f506424affda42dacc036ac4de18732bbe4c2d7
Alexandru Barbur <root.ctrlc@gmail.com>
2019-10-10 23:38:33 -0400

    Initial plugin instance worker implementation.

--- pkg/mdcs_node/src/plugin/instance.rs
+++ pkg/mdcs_node/src/plugin/instance.rs
@@ -1,13 +1,16 @@
 use std::io::{self, BufRead, BufReader, Result, ErrorKind};
 use std::net::TcpStream;
 use std::process::{Child, Command, Stdio};
+use std::sync::{Arc};
+use std::sync::mpsc::{channel, Sender, Receiver, TryRecvError};
+use std::thread::{self, JoinHandle};
+use std::time::Duration;
 
-use avro_rs::{from_avro_datum, from_value, to_avro_datum, Reader, Schema, Writer};
+use avro_rs::{from_value, Reader, Schema, Writer};
 
 use super::config::InstanceConfig;
-use super::request::{self as req, Request};
-use super::response::{self as resp, Response};
-
+use super::request::Request;
+use super::response::Response;
 
 fn initial_connect(child: &mut Child) -> Result<TcpStream> {
     let child_stdout = child
@@ -39,10 +42,24 @@ fn initial_connect(child: &mut Child) -> Result<TcpStream> {
     TcpStream::connect(address)
 }
 
+#[derive(Debug)]
 pub struct Instance {
     config: InstanceConfig,
     child: Child,
+    handle: JoinHandle<()>,
+    sender: Sender<Arc<InstanceMessage>>,
+}
+
+#[derive(Debug)]
+struct InstanceMessage {
+    request: Request,
+    response: Option<Response>
+}
+
+#[derive(Debug)]
+struct InstanceContext {
     stream: TcpStream,
+    receiver: Receiver<Arc<InstanceMessage>>,
 }
 
 impl Instance {
@@ -52,11 +69,90 @@ impl Instance {
             .spawn()?;
 
         let stream = initial_connect(&mut child)?;
+        let (sender, receiver) = channel();
+        let mut context = InstanceContext {
+            stream,
+            receiver
+        };
+
+        let handle = thread::spawn(move || {
+            context.run();
+        });
 
         Ok(Instance {
             config,
             child,
-            stream,
+            handle,
+            sender,
         })
+    }
+
+    pub fn process_request(&mut self, request: Request) -> Response {
+        let message = Arc::new(InstanceMessage {
+            request,
+            response: None
+        });
+
+        self.sender.send(message.clone()).expect("Failed to send message");
+
+        // TODO: wait for command to finish or time out
+        thread::sleep(Duration::from_millis(1000 * 5));
+
+        let message = Arc::try_unwrap(message).expect("Existing references to Arc");
+        message.response.expect("Failed to process request")
+    }
+}
+
+impl InstanceContext {
+    fn run(&mut self) {
+        // parse the plugin message schemas
+        let request_schema = Schema::parse_str(include_str!("request.avsc"))
+            .expect("Failed to parse request message schema");
+
+        let response_schema = Schema::parse_str(include_str!("response.avsc"))
+            .expect("Failed to parse response message schema");
+
+        // create request writer and response reader
+        let mut writer = Writer::new(&request_schema, &self.stream);
+        let mut reader = Reader::with_schema(&response_schema, &self.stream)
+            .expect("Failed to create Avro response reader");
+
+        // process requests
+        loop {
+            let mut message = match self.receiver.try_recv() {
+                Ok(message) => message,
+                Err(TryRecvError::Empty) => {
+                    // rate limit
+                    thread::sleep(Duration::from_millis(100));
+
+                    // try again
+                    continue;
+                },
+                Err(TryRecvError::Disconnected) => {
+                    // time to quit
+                    break;
+                }
+            };
+
+            // XXX: not sure if this is the best way
+            let message = Arc::get_mut(&mut message)
+                .expect("Failed to get mutable message reference");
+
+            // send the request
+            writer.append_ser(&message.request).expect("Failed to serialize request");
+            writer.flush().expect("Failed to flush writer");
+
+            // receive the response
+            let value = reader
+                .next()
+                .expect("Failed to read response")
+                .expect("Failed to unserialize response");
+
+            message.response = Some(
+                from_value::<Response>(&value).expect("Failed to unserialize response")
+            );
+
+            // TODO: notify whoever is waiting the command is complete
+        }
     }
 }
